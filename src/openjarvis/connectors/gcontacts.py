569bb2e6783efe7c7fe82bbcc0b639224a27b1c5
enkayxyz <nirav@kanakia.org>
2026-06-04 12:48:33 -0400

    Harden Google alias credential isolation

--- src/openjarvis/connectors/gcontacts.py
+++ src/openjarvis/connectors/gcontacts.py
@@ -157,9 +157,14 @@ class GContactsConnector(BaseConnector):
 
     def __init__(self, credentials_path: str = "", account: str = "") -> None:
         self._account = normalize_account_alias(account)
+        self._explicit_credentials_path = bool(credentials_path)
         self._credentials_path = resolve_google_credentials(
             credentials_path or _DEFAULT_CREDENTIALS_PATH,
-            account=self._account if not credentials_path else "",
+            account=self._account,
+            allow_shared_fallback=not self._explicit_credentials_path,
+        )
+        self._mirror_shared_credentials = (
+            not self._explicit_credentials_path and not self._account
         )
         self._items_synced: int = 0
         self._items_total: int = 0
